b408101aec6fa0fe94674579f005006d4a29fc3c
codingstar <codingyjt@gmail.com>
2015-07-17 21:36:40 +0800

    init2

--- develop/js/main.js
+++ develop/js/main.js
@@ -1,16 +1,16 @@
 /**
  * 简易的鼠标滚轮方向判断方法
- * 1：向上  -1：向下
+ * -1：向上  1：向下
  */
  
 var scrollFunc = function(e) {
     var direct=0;
     e=e || window.event;
-    console.log(e.deltaY);
+    //console.log(e.deltaY);
     if(e.wheelDelta){//IE/Opera/Chrome
-        return e.wheelDelta/120; 
+        return -e.wheelDelta/120; 
     }else if(e.detail){//Firefox
-        return -e.detail/3;
+        return e.detail/3;
     }
     return 0;
 }
